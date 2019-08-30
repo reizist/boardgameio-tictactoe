45d59ab7ec7eb7c05465c6edf146b526da366b93
reizist <reizist@gmail.com>
2019-08-31 02:55:23 +0900

    Use firestore for store

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
-const Server = require('boardgame.io/server').Server;
+const { Server, Firebase } = require('boardgame.io/server');
 const TicTacToe = require('./game').TicTacToe;
-const server = Server({ games: [TicTacToe]});
+const admin = require('firebase-admin');
+const serviceAccount = require('../credentials/serviceAccountKey.json');
+const server = Server({
+  games: [TicTacToe],
+  db: new Firebase({
+    config: {
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: 'https://tictactoe-1f098.firebaseio.com/',
+    },
+    engine: 'Firestore',
+    adminClient: true,
+  }),
+});
 
 server.run(8000);
